3709f40f2df97c44224ee1749a735e45a2b1dfc3
Adam Randlett <adam@monkdev.com>
2013-04-05 12:06:19 -0600

    Update to startup images.

--- app.js
+++ app.js
@@ -11,25 +11,25 @@ Ext.Loader.setPath({
   Ext.application({
       name: 'MonkTouch',
       appFolder: 'app',
-      
+
       icon:{
         57:   'resources/icons/icon.png',
         72:   'resources/icons/icon~ipad.png',
         114:  'resources/icons/icon@2x.png',
         144:  'resources/icons/icon~ipad@2x.png'
       },
-      
+
       startupImage:{
-        '320x460':  'resources/loading/Homescreen.jpg',
-        '640x920':  'resources/loading/Homescreen.jpg',
-        '640x1069': 'resources/loading/Homescreen.jpg'
+        '320x460':  'resources/loading/320x460.png',
+        '640x920':  'resources/loading/640x960.png',
+        '640x1136': 'resources/loading/640x1136.png'
       },
-      
+
       requires:[
        'MonkTouch.setup',
        'MonkTouch.FilterUtil'
       ],
-      
+
       controllers:[
         'NavView',
         'Viewport',
